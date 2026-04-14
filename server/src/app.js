2093a09bcf5eb598587ced99a41257d3e5439858
RohansinhVihol <rohansinhvihol06@gmail.com>
2026-04-14 12:28:09 +0530

    fix bug

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,11 @@ const app = express();
 
  
 
-app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN ,
-    //origin:"http://localhost:5173",
-    credentials : true,
-}))
+app.use(cors())
+    // origin: process.env.ALLOWED_ORIGIN ,
+    // //origin:"http://localhost:5173",
+    // credentials : true,
+
 
 app.use(express.json({limit :"16kb"}))
 
