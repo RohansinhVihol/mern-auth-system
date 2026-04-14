5e75991d6e077f0c1da91e77e2991688084963ba
RohansinhVihol <rohansinhvihol06@gmail.com>
2026-04-14 12:45:10 +0530

    access allowed origin

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,15 @@ const app = express();
 
  
 
-app.use(cors())
-    origin: process.env.ALLOWED_ORIGIN ,
-    // //origin:"http://localhost:5173",
-    // credentials : true,
+// app.use(cors())
+//     origin: process.env.ALLOWED_ORIGIN,
+//     // //origin:"http://localhost:5173",
+//     credentials : true,
+
+app.use(cors({
+  origin:process.env.ALLOWED_ORIGIN,
+  credentials: true
+}));
 
 
 app.use(express.json({limit :"16kb"}))
